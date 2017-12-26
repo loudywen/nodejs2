7e67ad2a1dd20610f6283a74df4ef358122c1542
loudywen <loudywen@gmail.com>
2017-12-26 13:34:54 -0500

    added mlab for mongodb

--- config/key.js
+++ config/key.js
@@ -1,5 +1,6 @@
 module.exports={
-    mongoURI:'mongodb://localhost/nodejs2-dev',
+   // mongoURI:'mongodb://localhost/nodejs2-dev',
+    mongoURI:'mongodb://testuser:testpassword@ds163796.mlab.com:63796/nodejs2-dev',
     googleClientID:'873296300564-5o8lh52avaeg24f3tk988mghlujlmacn.apps.googleusercontent.com',
     googleClientSecret:'cj1TswrVzLq5Tq7HWQmhtuK5'
 }
